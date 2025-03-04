2cd8e2f361f534fa8b7d1a3b4af81d368a8fe234
Din <Din.Salehy@triosstudent.com>
2025-03-04 08:08:53 -0500

    Initial Commit

---
+++ VideoPlayerApp/VideoPlayerAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  VideoPlayerAppApp.swift
+//  VideoPlayerApp
+//
+//  Created by Din Salehy on 2025-03-04.
+//
+
+import SwiftUI
+
+@main
+struct VideoPlayerAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
